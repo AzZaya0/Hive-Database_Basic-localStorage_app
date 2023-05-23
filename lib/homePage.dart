b986bf3f2d4b6288aff7fd3c93f9f8ac3549129a
AzZaya0 <ajaya.ajyol112@gmail.com>
2023-05-23 13:29:45 +0545

    new

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: sort_child_properties_last, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 class HomeApp extends StatefulWidget {
@@ -12,6 +14,12 @@ class HomeApp extends StatefulWidget {
 
 class _HomeAppState extends State<HomeApp> {
   final _mybox = Hive.box('mybox');
+  final _textcontroller = TextEditingController(); // to control the text filed
+
+  void WriteTextdata() {
+    _mybox.put('textInput', _textcontroller.text);
+    setState(() {});
+  }
 
   void Writedata() {
     _mybox.put(2, {'Azzaya1': 181, "yoho1": "brook1"});
@@ -36,6 +44,42 @@ class _HomeAppState extends State<HomeApp> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Container(
+              child: (_mybox.get('textInput') == null)
+                  // If data is null
+                  ? Text('')
+                  : Text(_mybox
+                      .get('textInput')
+                      .toString()), //to show the data from database
+            ),
+            Container(
+                padding: EdgeInsets.all(12),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextField(
+                        controller: _textcontroller,
+                        decoration: InputDecoration(
+                            hintText: "write here",
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(12))),
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                _textcontroller.clear();
+                              },
+                              icon: Icon(Icons.clear),
+                            )),
+                      ),
+                    ])),
+            MaterialButton(
+              onPressed: WriteTextdata,
+              child: Text(
+                'Write From TextField',
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Colors.pink,
+            ),
             MaterialButton(
               onPressed: Writedata,
               child: Text(
